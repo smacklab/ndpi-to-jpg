687963c8ddabdaf950c5ec0e3c606c6ed7b0806c
dnhuan <huandngoc@gmail.com>
2023-01-29 17:03:45 -0700

    Swapped row and column in filename

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ for filename in os.listdir("data"):
         w, h = image.size
         print("Image size: {}x{}".format(w, h))
         image_name = os.path.splitext(filename)[0]
-        # split image into 512x512 tiles, row by row
+        # split image into 512x512 tiles, column by column
         # and save them to "output" directory
-        # with name "row_column.jpg"
-        for r in tqdm(range(0, h, 512)):
-            for c in tqdm(range(0, w, 512), leave=False):
+        # with name 'image_name_column_row.jpg'
+        for c in tqdm(range(0, w, 512)):
+            for r in tqdm(range(0, h, 512), leave=False):
                 im1 = image.crop((c, r, c + 512, r + 512))
-                im1.save("output/" + image_name + "_" + str(r) + "_" + str(c) + ".jpg",'JPEG', quality=100)
+                im1.save("output/" + image_name + "_" + str(c) + "_" + str(r) + ".jpg",'JPEG', quality=100)
